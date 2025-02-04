2b78cc6c4e2a9cd005b179c6378114fa4d83451f
chaeeunj <official2930@naver.com>
2025-02-04 17:41:06 +0900

    page & layout

---
+++ next-tutorial/app/dashboard/layout.tsx
@@ -0,0 +1,7 @@
+export default function DashBoardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>대시보드 레이아웃 {children}</div>;
+}
---
+++ next-tutorial/app/dashboard/page.tsx
@@ -0,0 +1,3 @@
+export default function DashBoardPage() {
+  return <div>대시보드 페이지</div>;
+}
